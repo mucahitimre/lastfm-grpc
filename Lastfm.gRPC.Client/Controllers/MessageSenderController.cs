941c0a1a1b5f445255138baaaeade1d63d81bda7
Mücahit imre <mucahit.imre@gmail.com>
2022-05-15 15:49:34 +0300

    grpc streaming lastfm loved tracks data

--- Lastfm.gRPC.Client/Controllers/MessageSenderController.cs
+++ Lastfm.gRPC.Client/Controllers/MessageSenderController.cs
@@ -1,5 +1,6 @@
 ﻿using LastfmProtosClient;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace Lastfm.gRPC.Client.Controllers
 {
@@ -11,8 +12,8 @@ namespace Lastfm.gRPC.Client.Controllers
         {
         }
 
-        [HttpPost(Name = "GetLovedTracks")]
-        public object GetLovedTracks()
+        [HttpGet("UnaryGetLovedTracks")]
+        public object UnaryGetLovedTracks()
         {
             using var GrpcChannel = Grpc.Net.Client.GrpcChannel.ForAddress("https://localhost:7066");
             var client = new LastfmService.LastfmServiceClient(GrpcChannel);
@@ -20,6 +21,25 @@ namespace Lastfm.gRPC.Client.Controllers
             var tracks = response.ResponseAsync.Result;
 
             return tracks;
+        }
+
+        [HttpGet("StreamGetLovedTracks")]
+        public async Task StreamGetLovedTracks()
+        {
+            using var GrpcChannel = Grpc.Net.Client.GrpcChannel.ForAddress("https://localhost:7066");
+            var client = new LastfmService.LastfmServiceClient(GrpcChannel);
+            var response = client.GetLovedTracksStream(new LovedTrackStreamRequest { Count = 1 });
+
+            Response.ContentType = "application/json";
+            StreamWriter sw;
+            await using ((sw = new StreamWriter(Response.Body)).ConfigureAwait(false))
+            {
+                while (await response.ResponseStream.MoveNext(new CancellationToken(false)))
+                {
+                    await sw.WriteLineAsync(JsonConvert.SerializeObject(response.ResponseStream.Current)).ConfigureAwait(false);
+                    await sw.FlushAsync().ConfigureAwait(false);
+                }
+            }
         }
     }
 }
--- Lastfm.gRPC.Server/Services/LastfmServiceImpl.cs
+++ Lastfm.gRPC.Server/Services/LastfmServiceImpl.cs
@@ -4,39 +4,67 @@ using LastfmProtos;
 
 public class LastfmServiceImpl : LastfmService.LastfmServiceBase
 {
-    public override Task<LovedTrackResponse> GetLovedTracks(LovedTrackRequest request, ServerCallContext context)
+    public override async Task GetLovedTracksStream(LovedTrackStreamRequest request, IServerStreamWriter<LovedTrackResponse> responseStream, ServerCallContext context)
+    {
+        var tracks = LastfmServiceClient.UserGetLovedTracks().Lovedtracks;
+        var packageSize = tracks.Track.Count / request.Count;
+        for (int i = 0; i < packageSize;)
+        {
+            var data = tracks.Track.Skip(i).Take(request.Count);
+            i += request.Count;
+
+            var response = GetLovedTracksInternal(data, tracks.Attr);
+
+            await responseStream.WriteAsync(new LovedTrackResponse
+            {
+                Lovedtracks = response
+            });
+            Thread.Sleep(250);
+        }
+    }
+
+    public override async Task<LovedTrackResponse> GetLovedTracks(LovedTrackRequest request, ServerCallContext context)
     {
         var tracks = LastfmServiceClient.UserGetLovedTracks();
 
-        var model = new LovedTrackResponse();
+        var response = new LovedTrackResponse
+        {
+            Lovedtracks = GetLovedTracksInternal(tracks.Lovedtracks.Track, tracks.Lovedtracks.Attr)
+        };
+
+        return await Task.FromResult(response);
+    }
+
+    private static Lovedtracks GetLovedTracksInternal(IEnumerable<Lastfm.Client.Models.Track> tracks, Lastfm.Client.Models.Attr attr)
+    {
+        var model = new Lovedtracks();
         var list = new List<Track>();
-        tracks.Lovedtracks.Track.ForEach(e =>
+        foreach (var item in tracks)
         {
             var tr = new Track
             {
                 Artist = new Artist
                 {
-                    Name = e.Artist.Name,
-                    Mbid = e.Artist.Mbid,
-                    Url = e.Artist.Url.ToString()
+                    Name = item.Artist.Name,
+                    Mbid = item.Artist.Mbid,
+                    Url = item.Artist.Url.ToString()
                 },
                 Date = new Date
                 {
-                    Text = e.Date.Text,
-                    Uts = e.Date.Uts
+                    Text = item.Date.Text,
+                    Uts = item.Date.Uts
                 },
-                Mbid = e.Mbid,
-                Name = e.Name,
-                Url = e.Url.ToString(),
+                Mbid = item.Mbid,
+                Name = item.Name,
+                Url = item.Url.ToString(),
                 Streamable = new Streamable
                 {
-                    Fulltrack = e.Streamable.Fulltrack,
-                    Text = e.Streamable.Text
+                    Fulltrack = item.Streamable.Fulltrack,
+                    Text = item.Streamable.Text
                 }
-
             };
 
-            var images = e.Image.Select(e => new Image
+            var images = item.Image.Select(e => new Image
             {
                 Size = (Size)Enum.Parse(typeof(Size), e.Size.ToString()),
                 Text = e.Text.ToString()
@@ -45,22 +73,19 @@ public class LastfmServiceImpl : LastfmService.LastfmServiceBase
             tr.Image.AddRange(images);
 
             list.Add(tr);
-        });
+        }
 
-        model.Lovedtracks = new Lovedtracks()
+        model.Attr = new Attr
         {
-            Attr = new Attr
-            {
-                Page = tracks.Lovedtracks.Attr.Page,
-                PerPage = tracks.Lovedtracks.Attr.PerPage,
-                Total = tracks.Lovedtracks.Attr.Total,
-                TotalPages = tracks.Lovedtracks.Attr.TotalPages,
-                User = tracks.Lovedtracks.Attr.User
-            }
+            Page = attr.Page,
+            PerPage = attr.PerPage,
+            Total = attr.Total,
+            TotalPages = attr.TotalPages,
+            User = attr.User
         };
 
-        model.Lovedtracks.Track.AddRange(list);
+        model.Track.AddRange(list);
 
-        return Task.FromResult(model);
+        return model;
     }
 }
